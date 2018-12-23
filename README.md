7589227d2ece213732bbe4c088059085038df6ea
John Williamson <johnjwilliamson@live.com>
2018-12-23 17:27:48 +1000

    intial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# apollo-link-eosio
+
+# ⚠️ W.I.P️️ ️⚠️
+
+An Apollo Link to redirect GraphQL mutations to an EOSIO transaction
+
+## Purpose
+
+Apollo is a popular gateway to querying GraphQL servers however EOSIO based dApps typically mutate data on chain via transactions. **apollo-link-eosio** allows the developer write GraphQL mutations which are executed as EOSIO transactions.
+
+## Installation
+
+`apollo-link`, `graphql` and `eosjs` are peer dependencies needed by `apollo-link-eosio`.
---
+++ src/eosLink.ts
@@ -0,0 +1,31 @@
+export namespace EosLink {
+  export type ActionName = string;
+  export type ContractName = string;
+  export type AuthActor = string;
+  export type AuthPermission = string;
+
+  export interface Auth {
+    actor: AuthActor;
+    permission: AuthPermission;
+  }
+
+  export interface Options {
+    eosjs: any;
+    defaultContractAccount?: string;
+    defaultAuthActor?: string;
+    defaultAuthPermission?: string;
+  }
+
+  export interface DirectiveOptions {
+    name: ActionName;
+    actor?: AuthActor;
+    permission?: AuthPermission;
+  }
+
+  export interface Action {
+    name: ActionName;
+    account: ContractName;
+    authorization: Auth[];
+    data: any;
+  }
+}
